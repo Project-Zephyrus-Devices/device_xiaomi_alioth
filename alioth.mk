24af64b1ddf59af11bee6614eb5991a39e56c8a7
VEN0M98 <bt98.bt7@gmail.com>
2022-05-28 19:24:58 +0000

    alioth:welcome to zephyrus

--- alioth.mk
+++ alioth.mk
@@ -8,20 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_MODEL := M2102K1C
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Project Zephyrua stuff.
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
 TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+TARGET_INCLUDE_WIFI_EXT := true
+
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
